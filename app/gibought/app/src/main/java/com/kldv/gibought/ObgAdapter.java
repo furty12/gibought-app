07ace3504ac35945253938e5f646d5198e1d79d1
Diogo Pacheco <furtycraft@gmail.com>
2020-09-19 13:20:52 -0300

    1.0

---
+++ app/gibought/app/src/main/java/com/kldv/gibought/ObgAdapter.java
@@ -0,0 +1,63 @@
+package com.kldv.gibought;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class ObgAdapter extends RecyclerView.Adapter < ObgAdapter.ObgViewHolder > {
+
+    private List < ObgItem > obgItems;
+
+    public ObgAdapter(List < ObgItem > obgItems) {
+        this.obgItems = obgItems;
+    }
+
+    @NonNull
+    @Override
+    public ObgViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ObgViewHolder(
+                LayoutInflater.from(parent.getContext()).inflate(
+                        R.layout.item_container, parent, false
+                )
+        );
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ObgViewHolder holder, int position) {
+        holder.setObgData(obgItems.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return obgItems.size();
+    }
+
+    class ObgViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView textTitle;
+        private TextView textDesc;
+        private ImageView imgObg;
+
+        ObgViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textTitle = itemView.findViewById(R.id.textTitle);
+            textDesc = itemView.findViewById(R.id.textDescription);
+            imgObg = itemView.findViewById(R.id.imgObg);
+
+        }
+        void setObgData(ObgItem obgItem) {
+
+            textTitle.setText(obgItem.getTitle());
+            textDesc.setText(obgItem.getDesc());
+            imgObg.setImageResource(obgItem.getImg());
+
+        }
+    }
+}
